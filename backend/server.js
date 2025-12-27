368b94217c21d83d4bfc44087b70f49295d35d51
amandeepkumarflymediatech-star <amandeepkumar.flymediatech@gmail.com>
2025-12-27 10:33:55 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -83,7 +83,7 @@ const PORT = process.env.PORT || 5000;
 
 /* ================== CORS ================== */
 const allowedOrigins = [
-  "http://localhost:5173",
+  "http://localhost:5174",
   "https://www.theenglishraj.com",
 ];
 
